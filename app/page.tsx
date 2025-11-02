cd5456aaf01069ed387bb3e8f021137546d4e715
241111097@student.unu-jogja.ic.id <email>
2025-11-02 18:18:50 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,45 @@
 export default function HomePage() {
   return (
-    <main>
-      <header className="justify-between flex p-5 align-items-center display-flex">
-        <h1>Geng Centil</h1>
-        <ul>
+    // HOME PAGE
+    <div>
+      {/* NAVBAR */}
+      <nav className="justify-between flex p-5 align-items-center display-flex">
+        <h1 className="text-xl">Geng Centil</h1>
+        <ul className="flex text-sm justify-center gap-5 list-none">
           <li><a href="">HOME</a></li>
           <li><a href="">ABOUT</a></li>
           <li><a href="">MOMENTS</a></li>
           <li><a href="">PROGRAM</a></li>
         </ul>
-        <span>BOOKTRIP</span>
-      </header>
-    </main>
+        <span className="bg-black text-white text-sm px-5 py-2 rounded-2xl">BOOKTRIP</span>
+      </nav>
+      {/* NAVBAR END */}
+
+
+      <section>
+        <p>WELCOME TO WEBSITE GENG CENTIL</p>
+        <h2>LEARN, SHARE, AND BUILD TOGETHER</h2>
+      </section>
+    </div>
+    // HOME PAGE END
+
+
+
+    // MEET OUR TEAM
+
+    // MEET OUR TEAM END
+
+
+
+    // OUR MOMENTS
+
+    // OUR MOMENTS
+
+
+
+
+    // OUR PROGRAMS
+
+    // OUR PROGRAMS
   );
 }
